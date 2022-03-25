8c0a8d33d329b55d8d440b1c67431707c90e3b35
JadElHakim <jh.jadhakim@gmail.com>
2022-03-25 16:28:59 +0200

    set up calculation page

--- app/src/main/java/com/example/project1/CalculationPage.java
+++ app/src/main/java/com/example/project1/CalculationPage.java
@@ -1,4 +1,13 @@
 package com.example.project1;
 
-public class CalculationPage {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CalculationPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculation_page);
+    }
 }
