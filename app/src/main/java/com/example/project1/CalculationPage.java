729f8f32b3ce60981ebbdadcb2151478925125bb
JadElHakim <jh.jadhakim@gmail.com>
2022-03-25 16:38:09 +0200

    fixes

--- app/src/main/java/com/example/project1/CalculationPage.java
+++ app/src/main/java/com/example/project1/CalculationPage.java
@@ -1,13 +1,14 @@
 package com.example.project1;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
+
 public class CalculationPage extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.calculation_page);
+        setContentView(R.layout.activity_calculation_page);
     }
 }
