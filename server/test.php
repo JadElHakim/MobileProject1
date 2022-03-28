18663cc7d376ac0b82ec387a60e5961b1dbd5892
Alexander Abou Chedid <alexander.abouchedid@lau.edu>
2022-03-28 21:13:29 +0300

    api-work

--- server/test.php
+++ server/test.php
@@ -12,4 +12,25 @@ $hour=date("H");
 $today = (string)((int)($year)).(string)((int)($month)).(string)((int)($day)).(string)((int)($hour));
 echo $today;
 
+$ch=curl_init();
+
+$url="https://lirarate.org/wp-json/lirarate/v2/omt?currency=LBP&_ver=t".$today;
+
+echo $url;
+curl_setopt($ch,CURLOPT_URL,$url);
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+$response=curl_exec($ch);
+if($e=curl_error($ch)){
+    echo $e;
+}else{
+    $decoded=json_decode($response,true);
+    print_r($decoded);
+}
+
+        echo $decoded["omt"][114][1];
+    
+
+
+curl_close($ch);
+
 ?>
