6048ef13245b7c3e57b7db760e4ff21a149a1c10
Alexander Abou Chedid <alexander.abouchedid@lau.edu>
2022-03-28 19:50:50 +0300

    timestamp-server-test.php

--- server/test.php
+++ server/test.php
@@ -1,9 +1,15 @@
-<?php 
 
+<?php
+date_default_timezone_set('Asia/Beirut');  
 
+//storing variables this way because api requires to remove leading 0s and is of this format
+$year = date("Y");  
+$month=date("m");
+$day=date("d");
+$hour=date("H");
 
-
-
-
+//link fetch json object from 
+$today = (string)((int)($year)).(string)((int)($month)).(string)((int)($day)).(string)((int)($hour));
+echo $today;
 
 ?>
