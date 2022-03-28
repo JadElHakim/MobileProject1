4ff2b33adfe1ba1eed09132a1dd096f36f97bede
Alexander Abou Chedid <alexander.abouchedid@lau.edu>
2022-03-28 21:26:00 +0300

    rate-stored-in-api

--- server/test.php
+++ server/test.php
@@ -10,13 +10,12 @@ $hour=date("H");
 
 //link fetch json object from 
 $today = (string)((int)($year)).(string)((int)($month)).(string)((int)($day)).(string)((int)($hour));
-echo $today;
+
 
 $ch=curl_init();
 
 $url="https://lirarate.org/wp-json/lirarate/v2/omt?currency=LBP&_ver=t".$today;
 
-echo $url;
 curl_setopt($ch,CURLOPT_URL,$url);
 curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
 $response=curl_exec($ch);
@@ -24,11 +23,9 @@ if($e=curl_error($ch)){
     echo $e;
 }else{
     $decoded=json_decode($response,true);
-    print_r($decoded);
 }
-
-        echo $decoded["omt"][114][1];
-    
+        $latest_value=end($decoded["omt"]);
+        echo $latest_value[1];
 
 
 curl_close($ch);
