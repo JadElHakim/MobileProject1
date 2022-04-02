a7493ec653b7b4fa13aa7cddda2d5df9fed75e4a
JadElHakim <jh.jadhakim@gmail.com>
2022-04-02 11:36:29 +0300

    input design

--- frontend/app/src/main/java/com/example/project1/CalculationPage.java
+++ frontend/app/src/main/java/com/example/project1/CalculationPage.java
@@ -51,9 +51,9 @@ public class CalculationPage extends AppCompatActivity {
     //values to be stored as doubles then sent to the post api for calculation
     double usd_to_convert, lbp_to_convert, current_rate;
     //url for fetching the current rate
-    String url = "http://ADD-IP-HERE/project1/test.php";
+    String url = "http://192.168.1.105:8080/project1/test.php";
     //url for posting conversion result
-    String post_url = "http://ADD-IP-HERE/project1/post_data.php";
+    String post_url = "http://192.168.1.105:8080/project1/post_data.php";
     //fetching rate
     DownloadTask task;
     //posting conversion
